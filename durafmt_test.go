e37045b8c162acaa9534a534f4b8b42b6672ba31
Radu Coroi <rcoroi@gmail.com>
2018-04-25 22:23:15 +0100

    added short version as requested in https://github.com/hako/durafmt/issues/11

--- durafmt_test.go
+++ durafmt_test.go
@@ -62,6 +62,52 @@ func TestParse(t *testing.T) {
 	}
 }
 
+// TestParseShort for durafmt time.Duration conversion, short version.
+func TestParseShort(t *testing.T) {
+	testTimes = []struct {
+		test     time.Duration
+		expected string
+	}{
+		{1 * time.Millisecond, "1 millisecond"},
+		{1 * time.Second, "1 second"},
+		{1 * time.Hour, "1 hour"},
+		{1 * time.Minute, "1 minute"},
+		{2 * time.Millisecond, "2 milliseconds"},
+		{2 * time.Second, "2 seconds"},
+		{2 * time.Minute, "2 minutes"},
+		{1 * time.Hour, "1 hour"},
+		{2 * time.Hour, "2 hours"},
+		{10 * time.Hour, "10 hours"},
+		{24 * time.Hour, "1 day"},
+		{48 * time.Hour, "2 days"},
+		{120 * time.Hour, "5 days"},
+		{168 * time.Hour, "1 week"},
+		{672 * time.Hour, "4 weeks"},
+		{8759 * time.Hour, "52 weeks"},
+		{8760 * time.Hour, "1 year"},
+		{17519 * time.Hour, "1 year"},
+		{17520 * time.Hour, "2 years"},
+		{26279 * time.Hour, "2 years"},
+		{26280 * time.Hour, "3 years"},
+		{201479 * time.Hour, "22 years"},
+		{201480 * time.Hour, "23 years"},
+		{-1 * time.Second, "-1 second"},
+		{-10 * time.Second, "-10 seconds"},
+		{-100 * time.Second, "-1 minute"},
+		{-1 * time.Millisecond, "-1 millisecond"},
+		{-10 * time.Millisecond, "-10 milliseconds"},
+		{-100 * time.Millisecond, "-100 milliseconds"},
+	}
+
+	for _, table := range testTimes {
+		result := ParseShort(table.test).String()
+		if result != table.expected {
+			t.Errorf("Parse(%q).String() = %q. got %q, expected %q",
+				table.test, result, result, table.expected)
+		}
+	}
+}
+
 // TestParseString for durafmt duration string conversion.
 func TestParseString(t *testing.T) {
 	testStrings = []struct {
@@ -135,6 +181,90 @@ func TestParseString(t *testing.T) {
 
 	for _, table := range testStrings {
 		d, err := ParseString(table.test)
+		if err != nil {
+			t.Errorf("%q", err)
+		}
+		result := d.String()
+		if result != table.expected {
+			t.Errorf("d.String() = %q. got %q, expected %q",
+				table.test, result, table.expected)
+		}
+	}
+}
+
+// TestParseString for durafmt duration string conversion.
+func TestParseStringShort(t *testing.T) {
+	testStrings = []struct {
+		test     string
+		expected string
+	}{
+		{"1ms", "1 millisecond"},
+		{"2ms", "2 milliseconds"},
+		{"1s", "1 second"},
+		{"2s", "2 seconds"},
+		{"1m", "1 minute"},
+		{"2m", "2 minutes"},
+		{"1h", "1 hour"},
+		{"2h", "2 hours"},
+		{"10h", "10 hours"},
+		{"24h", "1 day"},
+		{"48h", "2 days"},
+		{"120h", "5 days"},
+		{"168h", "1 week"},
+		{"672h", "4 weeks"},
+		{"8759h", "52 weeks"},
+		{"8760h", "1 year"},
+		{"17519h", "1 year"},
+		{"17520h", "2 years"},
+		{"26279h", "2 years"},
+		{"26280h", "3 years"},
+		{"201479h", "22 years"},
+		{"201480h", "23 years"},
+		{"1m0s", "1 minute"},
+		{"1m2s", "1 minute"},
+		{"3h4m5s", "3 hours"},
+		{"6h7m8s9ms", "6 hours"},
+		{"0ms", "0 milliseconds"},
+		{"0s", "0 seconds"},
+		{"0m", "0 minutes"},
+		{"0h", "0 hours"},
+		{"0m1ms", "1 millisecond"},
+		{"0m1s", "1 second"},
+		{"0m1m", "1 minute"},
+		{"0m2ms", "2 milliseconds"},
+		{"0m2s", "2 seconds"},
+		{"0m2m", "2 minutes"},
+		{"0m2m3h", "3 hours"},
+		{"0m2m34h", "1 day"},
+		{"0m56h7m8ms", "2 days"},
+		{"-1ms", "-1 millisecond"},
+		{"-1s", "-1 second"},
+		{"-1m", "-1 minute"},
+		{"-1h", "-1 hour"},
+		{"-2ms", "-2 milliseconds"},
+		{"-2s", "-2 seconds"},
+		{"-2m", "-2 minutes"},
+		{"-2h", "-2 hours"},
+		{"-10h", "-10 hours"},
+		{"-24h", "-1 day"},
+		{"-48h", "-2 days"},
+		{"-120h", "-5 days"},
+		{"-168h", "-1 week"},
+		{"-672h", "-4 weeks"},
+		{"-8760h", "-1 year"},
+		{"-1m0s", "-1 minute"},
+		{"-0m2s", "-2 seconds"},
+		{"-0m2m", "-2 minutes"},
+		{"-0m2m3h", "-3 hours"},
+		{"-0m2m34h", "-1 day"},
+		{"-0ms", "-0 milliseconds"},
+		{"-0s", "-0 seconds"},
+		{"-0m", "-0 minutes"},
+		{"-0h", "-0 hours"},
+	}
+
+	for _, table := range testStrings {
+		d, err := ParseStringShort(table.test)
 		if err != nil {
 			t.Errorf("%q", err)
 		}
