852a0418eee564612ed819be8a7eeebf36e35602
Wesley Hill <wesley@hakobaito.co.uk>
2018-05-20 13:14:41 +0100

    fmt examples

--- example_test.go
+++ example_test.go
@@ -1,12 +1,12 @@
 package durafmt
 
 import (
+	"fmt"
 	"math"
 	"time"
-	"fmt"
 )
 
-func ExampleParseString()  {
+func ExampleParseString() {
 	duration, err := ParseString("354h22m3.24s")
 	if err != nil {
 		fmt.Println(err)
@@ -15,7 +15,7 @@ func ExampleParseString()  {
 	// duration.String() // String representation. "2 weeks 18 hours 22 minutes 3 seconds"
 }
 
-func ExampleParseString_sequence()  {
+func ExampleParseString_sequence() {
 	for hours := 1.0; hours < 12.0; hours++ {
 		hour := fmt.Sprintf("%fh", math.Pow(2, hours))
 		duration, err := ParseString(hour)
@@ -27,7 +27,7 @@ func ExampleParseString_sequence()  {
 }
 
 // Version of durafmt.ParseString() that only returns the first part of the duration string.
-func ExampleParseStringShort()  {
+func ExampleParseStringShort() {
 	duration, err := ParseStringShort("354h22m3.24s")
 	if err != nil {
 		fmt.Println(err)
@@ -36,14 +36,14 @@ func ExampleParseStringShort()  {
 	// duration.String() // String representation. "2 weeks 18 hours 22 minutes 3 seconds"
 }
 
-func ExampleParse()  {
+func ExampleParse() {
 	timeduration := (354 * time.Hour) + (22 * time.Minute) + (3 * time.Second)
 	duration := Parse(timeduration).String()
 	fmt.Println(duration) // 2 weeks 18 hours 22 minutes 3 seconds
 }
 
 // Version of durafmt.Parse() that only returns the first part of the duration string.
-func ExampleParseShort()  {
+func ExampleParseShort() {
 	timeduration := (354 * time.Hour) + (22 * time.Minute) + (3 * time.Second)
 	duration := ParseShort(timeduration).String()
 	fmt.Println(duration) // 2 weeks
