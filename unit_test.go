21b225ba3276e6ccc8c12a9b8fd76c44d733849d
Blake G <blakeblind@gmail.com>
2021-03-14 01:47:07 -0700

    Remove reflect package from unit_test.

--- unit_test.go
+++ unit_test.go
@@ -1,7 +1,6 @@
 package durafmt
 
 import (
-	"reflect"
 	"strconv"
 	"testing"
 )
@@ -73,9 +72,44 @@ func TestUnitsCoder_Decode(t *testing.T) {
 				t.Errorf("ParseUnits() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(gotU, tt.wantU) {
+			if !unitsEqual(gotU, tt.wantU) {
 				t.Errorf("ParseUnits() gotU = %v, want %v", gotU, tt.wantU)
 			}
 		})
 	}
+}
+
+func unitEqual(u1, u2 Unit) bool {
+	if u1.Singular != u2.Singular || u1.Plural != u2.Plural {
+		return false
+	}
+	return true
+}
+
+func unitsEqual(u1, u2 Units) bool {
+	if !unitEqual(u1.Year, u2.Year) {
+		return false
+	}
+	if !unitEqual(u1.Week, u2.Week) {
+		return false
+	}
+	if !unitEqual(u1.Day, u2.Day) {
+		return false
+	}
+	if !unitEqual(u1.Hour, u2.Hour) {
+		return false
+	}
+	if !unitEqual(u1.Minute, u2.Minute) {
+		return false
+	}
+	if !unitEqual(u1.Second, u2.Second) {
+		return false
+	}
+	if !unitEqual(u1.Millisecond, u2.Millisecond) {
+		return false
+	}
+	if !unitEqual(u1.Microsecond, u2.Microsecond) {
+		return false
+	}
+	return true
 }
